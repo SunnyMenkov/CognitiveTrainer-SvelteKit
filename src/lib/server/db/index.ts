617b8fe6fdbfebf26b7052c8825804bc552dd2fa
amenk <spyssasin@gmail.com>
2026-05-29 15:31:19 +0300

    feat neon db

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,10 +1,13 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
+// import { drizzle } from 'drizzle-orm/postgres-js';
+import { drizzle } from "drizzle-orm/neon-http";
+// import postgres from 'postgres';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
+import { neon } from '@neondatabase/serverless';
+
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
-const client = postgres(env.DATABASE_URL);
 
-export const db = drizzle(client, { schema });
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql, { schema });
